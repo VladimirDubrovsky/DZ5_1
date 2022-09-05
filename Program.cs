d29f7880267d96be69d313021e36a5f9b3167f38
Vladimir <v0vanchikanna@gmail.com>
2022-09-05 22:14:51 +0300

    1z

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿//  Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void FillArray(int[] collection)
+{ 
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i]=new Random().Next(100, 999);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.WriteLine(col[i]);
+    }
+}
+
+int count = new Random().Next(1, 40);
+int[] array = new int[count];
+
+FillArray(array);
+PrintArray(array);
+
+int number=0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]%2 == 0)
+    {
+        number++;
+    }
+}
+
+Console.WriteLine($"Колличество элементов в массиве: {count} , из них четных чисел в массиве: {number}");
